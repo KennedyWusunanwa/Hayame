a1d686d4cdbade667dd17b64202aca1a57046b6a
Wusunanwa <kennedywusunanwar@gmail.com>
2025-12-28 00:50:53 +0000

    Fix missing headers import in car detail page

--- src/app/cars/[id]/page.tsx
+++ src/app/cars/[id]/page.tsx
@@ -14,6 +14,7 @@ import type { Database } from "@/lib/database.types";
 import { createSupabaseServerClient } from "@/lib/supabase/server";
 import { mockCars } from "@/lib/mock-data";
 import { formatCurrency } from "@/lib/utils";
+import { headers } from "next/headers";
 
 export const dynamic = "force-dynamic";
 
